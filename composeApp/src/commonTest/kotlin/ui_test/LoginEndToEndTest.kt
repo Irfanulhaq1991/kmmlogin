5fe806989c24aca6f8b7edf463335c968a068cc0
Irfan <arfanulhaq1991@gmail.com>
2024-03-27 14:45:59 +0500

    Ui test are renamed

--- composeApp/src/commonTest/kotlin/ui_test/LoginEndToEndTest.kt
+++ composeApp/src/commonTest/kotlin/ui_test/LoginEndToEndTest.kt
@@ -1,48 +1,17 @@
 package ui_test
 
 import App
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.assertTextEquals
-import androidx.compose.ui.test.click
 import androidx.compose.ui.test.isDisplayed
-import androidx.compose.ui.test.isNotDisplayed
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performGesture
 import androidx.compose.ui.test.performImeAction
 import androidx.compose.ui.test.performTextInput
-import androidx.compose.ui.test.performTouchInput
 import androidx.compose.ui.test.runComposeUiTest
-import di.apiModule
-import di.shareMainModule
-import initKoin
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.TestDispatcher
-import kotlinx.coroutines.test.TestScope
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import org.koin.compose.getKoin
-import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
-import org.koin.dsl.koinApplication
 import org.koin.test.KoinTest
-import org.koin.test.mock.declare
-import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
@@ -50,6 +19,19 @@ import kotlin.test.Test
 
 // does works on iOS
 class LoginEndToEndTest:KoinTest {
+
+    @BeforeTest
+    fun setUp() {
+        startApp()
+    }
+
+    @AfterTest
+    fun tearDown() {
+        stopApp()
+    }
+
+
+
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun test_Loader_Displayed_On_Done_Taped() = runComposeUiTest {
@@ -69,7 +51,7 @@ class LoginEndToEndTest:KoinTest {
 
     @OptIn(ExperimentalTestApi::class)
     @Test
-    fun test_Loader_Displayed_On_Login_Button_Clicked() = runComposeUiTest {
+    fun `Loader is displayed is when login button clicked`() = runComposeUiTest {
         setContent {
             App()
         }
@@ -84,7 +66,7 @@ class LoginEndToEndTest:KoinTest {
     }
     @OptIn(ExperimentalTestApi::class)
     @Test
-    fun test_Error_Displayed_On_No_Inputs_And_Button_Clicked() = runComposeUiTest {
+    fun `Error is displayed when username and password is empty and login button is clicked`() = runComposeUiTest {
         setContent {
             App()
         }
@@ -94,7 +76,7 @@ class LoginEndToEndTest:KoinTest {
 
     @OptIn(ExperimentalTestApi::class)
     @Test
-    fun test_Register_Screen_Displayed_On_Register_Clicked() = runComposeUiTest {
+    fun `Register screen displayed when register label is clicked`() = runComposeUiTest {
         setContent {
             App()
         }
